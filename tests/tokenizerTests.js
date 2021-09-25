2ceeac5051ba2112b135d36aa1d73bb7005de9b6
Oliwer <oe222ez@student.lnu.se>
2021-09-25 16:37:21 +0200

    Add TC5-TC8 in tokenizerTests

--- tests/tokenizerTests.js
+++ tests/tokenizerTests.js
@@ -47,10 +47,65 @@ const tc4 = () => {
   wordAndDotTokenizer.getNextToken()
   wordAndDotTokenizer.getNextToken()
   if (wordAndDotTokenizer.getActiveToken().value === 'b' && wordAndDotTokenizer.getActiveToken().tokenType === 'WORD') {
-    console.log('TC3: PASS')
+    console.log('TC4: PASS')
   } else {
-    console.log('TC3: FAIL, expected value: "b" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "WORD" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+    console.log('TC4: FAIL, expected value: "b" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "WORD" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
   }
 }
 tc4()
+
+const tc5 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer('aa. b')
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getNextToken()
+  if (wordAndDotTokenizer.getActiveToken().value === 'b' && wordAndDotTokenizer.getActiveToken().tokenType === 'WORD') {
+    console.log('TC5: PASS')
+  } else {
+    console.log('TC5: FAIL, expected value: "b" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "WORD" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc5()
+
+const tc6 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer('a .b')
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getPrevToken()
+  if (wordAndDotTokenizer.getActiveToken().value === '.' && wordAndDotTokenizer.getActiveToken().tokenType === 'DOT') {
+    console.log('TC6: PASS')
+  } else {
+    console.log('TC6: FAIL, expected value: "." got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "DOT" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc6()
+
+const tc7 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer('')
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getPrevToken()
+  if (wordAndDotTokenizer.getActiveToken().value === '' && wordAndDotTokenizer.getActiveToken().tokenType === 'END') {
+    console.log('TC7: PASS')
+  } else {
+    console.log('TC7: FAIL, expected value: "" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "END" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc7()
+
+const tc8 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer(' ')
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getNextToken()
+  wordAndDotTokenizer.getPrevToken()
+  if (wordAndDotTokenizer.getActiveToken().value === '' && wordAndDotTokenizer.getActiveToken().tokenType === 'END') {
+    console.log('TC8: PASS')
+  } else {
+    console.log('TC8: FAIL, expected value: "" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "END" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc8()
 
