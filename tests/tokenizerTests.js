73c8f5c551eb28bb1df2553f17ee0e33b1aa9b64
Oliwer <oe222ez@student.lnu.se>
2021-09-25 16:45:46 +0200

    Add TC9-TC14 in tokenizerTests

--- tests/tokenizerTests.js
+++ tests/tokenizerTests.js
@@ -2,10 +2,6 @@ import { Tokenizer } from "../src/tokenizer.js"
 import { wordAndDotGrammar } from '../src/grammars/wordAndDotGrammar.js'
 import { arithmeticGrammar } from '../src/grammars/arithmeticGrammar.js'
 
-
-
-const arithmeticTokenizer = new Tokenizer(arithmeticGrammar)
-
 const tc1 = () => {
   const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
   wordAndDotTokenizer.startTokenizer('a')
@@ -108,4 +104,78 @@ const tc8 = () => {
   }
 }
 tc8()
+
+const tc9 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer('a')
+  wordAndDotTokenizer.getNextToken()
+  if (wordAndDotTokenizer.getActiveToken().value === '' && wordAndDotTokenizer.getActiveToken().tokenType === 'END') {
+    console.log('TC9: PASS')
+  } else {
+    console.log('TC9: FAIL, expected value: "" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "END" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc9()
+
+const tc10 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer('a')
+  wordAndDotTokenizer.getPrevToken()
+  if (wordAndDotTokenizer.getActiveToken().value === '' && wordAndDotTokenizer.getActiveToken().tokenType === 'END') {
+    console.log('TC10: PASS')
+  } else {
+    console.log('TC10: FAIL, expected value: "" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "END" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc10()
+
+// Ska det vara lexikalfel? förväntat är Exception i listan!!
+const tc11 = () => {
+  const wordAndDotTokenizer = new Tokenizer(wordAndDotGrammar)
+  wordAndDotTokenizer.startTokenizer('!')
+  wordAndDotTokenizer.getPrevToken()
+  if (wordAndDotTokenizer.getActiveToken().value === 'No lexical element matches "!"' && wordAndDotTokenizer.getActiveToken().tokenType === 'Lexikalfel') {
+    console.log('TC11: PASS')
+  } else {
+    console.log('TC11: FAIL, expected value: "No lexical element matches "!"" got: ' + wordAndDotTokenizer.getActiveToken().value + ' expected token type: "Lexikalfel" got: ' + wordAndDotTokenizer.getActiveToken().tokenType)
+  }
+}
+tc11()
+
+const tc12 = () => {
+  const arithmeticTokenizer = new Tokenizer(arithmeticGrammar)
+  arithmeticTokenizer.startTokenizer('3')
+  if (arithmeticTokenizer.getActiveToken().value === '3' && arithmeticTokenizer.getActiveToken().tokenType === 'NUMBER') {
+    console.log('TC12: PASS')
+  } else {
+    console.log('TC12: FAIL, expected value: "3" got: ' + arithmeticTokenizer.getActiveToken().value + ' expected token type: "NUMBER" got: ' + arithmeticTokenizer.getActiveToken().tokenType)
+  }
+}
+tc12()
+
+const tc13 = () => {
+  const arithmeticTokenizer = new Tokenizer(arithmeticGrammar)
+  arithmeticTokenizer.startTokenizer('3.14')
+  if (arithmeticTokenizer.getActiveToken().value === '3.14' && arithmeticTokenizer.getActiveToken().tokenType === 'NUMBER') {
+    console.log('TC13: PASS')
+  } else {
+    console.log('TC13: FAIL, expected value: "3.14" got: ' + arithmeticTokenizer.getActiveToken().value + ' expected token type: "NUMBER" got: ' + arithmeticTokenizer.getActiveToken().tokenType)
+  }
+}
+tc13()
+
+const tc14 = () => {
+  const arithmeticTokenizer = new Tokenizer(arithmeticGrammar)
+  arithmeticTokenizer.startTokenizer('3 + 54 * 4')
+  arithmeticTokenizer.getNextToken()
+  arithmeticTokenizer.getNextToken()
+  arithmeticTokenizer.getNextToken()
+  if (arithmeticTokenizer.getActiveToken().value === '*' && arithmeticTokenizer.getActiveToken().tokenType === 'MUL') {
+    console.log('TC14: PASS')
+  } else {
+    console.log('TC14: FAIL, expected value: "*" got: ' + arithmeticTokenizer.getActiveToken().value + ' expected token type: "MUL" got: ' + arithmeticTokenizer.getActiveToken().tokenType)
+  }
+}
+tc14()
+
 
